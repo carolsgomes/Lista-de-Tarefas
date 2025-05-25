36d8aa631a454b2be3f67a9224d223402e7b313a
carolsgomes <anasgcarolina@gmail.com>
2025-05-24 23:32:40 -0300

    Funcoes dos botoes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,5 @@
 //////////////LISTA DE TAREFAS////////////////
-// Selecao de elementos
+//// Selecao de elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
@@ -7,8 +7,10 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
 
-// Funcoes
+
+//// Funcoes
 const saveTodo = (text) => {
   const todo = document.createElement("div");
   todo.classList.add("todo");
@@ -38,8 +40,27 @@ const saveTodo = (text) => {
   todoInput.focus();  //focar no input tarefa
 };
 
+const toggleForms = () => { // faz aparecer somente o edit
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+}
 
-// Eventos
+const updateTodo = (text) => { //funcao do atualizar
+
+  const todos = document.querySelectorAll(".todo"); //seleciona tds
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3"); //pega o titulo mapeado
+
+    if (todoTitle.innerText === oldInputValue) {//comparando titulos e alterando
+      todoTitle.innerText = text;
+    } 
+
+  })
+}
+
+///// Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -56,9 +77,53 @@ document.addEventListener("click", (e) => {
 
   const targetEl = e.target;
   const parentEl = targetEl.closest("div"); //elemento pai e div mais prox
-
-  if (targetEl.classList.constains("finish-todo")) {
-    console.log("Click para finalizar"); //verifica clica do check
-
+  
+  let todoTitle; //mapear título para edição
+  if(parentEl && parentEl.querySelector("h3")) { //verificar titulo
+    todoTitle = parentEl.querySelector("h3").innerText; //busca p titulo
   }
-})
+  
+  //done - tarefa concluida
+  if (targetEl.classList.contains("finish-todo")) { 
+    parentEl.classList.toggle("done"); //desmarcar
+    } 
+    
+    //edit - editar
+    if (targetEl.classList.contains("edit-todo")) {
+      toggleForms();
+      
+      //salvando variaveis
+      editInput.value = todoTitle;
+      oldInputValue = todoTitle; 
+    } 
+
+    //remove - remover 
+    if (targetEl.classList.contains("remove-todo")) {
+      parentEl.remove();
+      
+      
+    }      
+});
+
+//salvando o edit
+editForm.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if (editInputValue) {//atualizar
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+
+}) 
+
+//cancelar - não processeguir com o formulario
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  toggleForms();
+});
+
